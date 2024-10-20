28e70dd3a3b4e172c855c62331f81b1cf5cce8c4
Robert Salmon <robsalmon@users.noreply.github.com>
2024-10-20 19:19:34 +0000

    Add file illustrating using comments to create a web server

---
+++ comments.js
@@ -0,0 +1,10 @@
+//Create web server
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+//Create comments array
+const comments = [
+    { username: 'Tammy', comment: 'lololol' },
+    { username: 'BoB', comment: 'lmao' },
+];
